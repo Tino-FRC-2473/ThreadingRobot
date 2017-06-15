b66b8428f9da85d817427cf9384f5bffb49b990a
kashyappanda <kashyap.panda@gmail.com>
2017-06-15 11:17:16 -0700

    Added TRCommand and TRSubsystem
    Allows for easier command and subsystem specific customization

---
+++ src/org/usfirst/frc/team2473/subsystems/TRSubsystem.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team2473.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class TRSubsystem extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        
+    }
+}
+
